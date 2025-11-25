c57c5c4e8ad3a66045fd0d124de5db590b5fb4eb
Randy2222222 <randyjpollitt@yahoo.com>
2025-11-25 14:27:55 -0500

    Update game.js

--- game.js
+++ game.js
@@ -540,18 +540,37 @@ function explodeShip(ship) {
         if (removed) continue;
         // saucers
         for (let s = saucers.length - 1; s >= 0; s--) {
-          // Replace next section of code 🔥 n
+          // Replace next section of code 🔥
           if (dist(b.x, b.y, saucers[s].x, saucers[s].y) < saucers[s].r) {
-            explodeAt(saucers[s].x, saucers[s].y, 16);
-            score += SAUCER_SCORE;
-            bullets.splice(i, 1);
+    const dying = saucers[s];
+    if (dying.sound) {
+        try { dying.sound.src.stop(); } catch(e){}
+        dying.sound = null;
+    }
+
+    explodeAt(dying.x, dying.y, 16);
+    score += SAUCER_SCORE;
+
+    bullets.splice(i, 1);
+    saucers.splice(s, 1);
+
+    if (buffers.explode) playBuffer("explode", V.explodeGain, false);
+    removed = true;
+    break;
+} // end new code 🔥 
+          // comment out old cold ⬇️
+       //   if (dist(b.x, b.y, saucers[s].x, saucers[s].y) < saucers[s].r) {
+        //    explodeAt(saucers[s].x, saucers[s].y, 16);
+          //   score += SAUCER_SCORE;
+          //  bullets.splice(i, 1);
             // stop saucer sound
-            stopActiveSaucerSound();
-            saucers.splice(s, 1);
-            if (buffers.explode) playBuffer("explode", V.explodeGain, false);
-            removed = true;
-            break;
-          }
+         //   stopActiveSaucerSound();
+          //  saucers.splice(s, 1);
+         //   if (buffers.explode) playBuffer("explode", V.explodeGain, false);
+        //    removed = true;
+         //   break;
+        //  }
+          // end commenting out old code ⬆️
         }
       }
 
