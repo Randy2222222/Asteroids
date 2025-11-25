5e8c5006bf48aa9fea4fe04d9b151dffecd7f87c
Randy2222222 <randyjpollitt@yahoo.com>
2025-11-25 01:42:22 -0500

    Update game.js

--- game.js
+++ game.js
@@ -549,7 +549,8 @@ function explodeShip(ship) {
         for (let i = saucerBullets.length - 1; i >= 0; i--) {
           const sb = saucerBullets[i];
           if (dist(sb.x, sb.y, ship.x, ship.y) < ship.r) {
-            explodeAt(ship);
+            explodeAt(ship);// replaced from line below,
+            //explodeAt(ship.x, ship.y, 16);
             ship.lives--;
             ship.x = w / 2; ship.y = h / 2; ship.vx = 0; ship.vy = 0;
             ship.invuln = 240; // kept for gameplay fairness
@@ -564,7 +565,8 @@ function explodeShip(ship) {
       if (ship.invuln <= 0) { 
         for (let i = asteroids.length - 1; i >= 0; i--) {
           if (dist(ship.x, ship.y, asteroids[i].x, asteroids[i].y) < ship.r + asteroids[i].r) {
-            explodeAt(ship);
+            explodeAt(ship);// replaced from line below
+            //explodeAt(ship.x, ship.y, 20);
             ship.lives--;
             ship.x = w / 2; ship.y = h / 2; ship.vx = 0; ship.vy = 0;
             ship.invuln = 90;
